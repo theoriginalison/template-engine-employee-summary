8069d4549212dd9b899c9e3788c8160471021ae8
Alison Lubar <aslubar@gmail.com>
2020-10-30 19:19:12 -0400

    finished employee JS file

--- app.js
+++ app.js
@@ -10,6 +10,37 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Enter employee name.",
+    },
+    {
+        type: "list",
+        message: "Select employee role.",
+        name: "role",
+        choice: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ]
+    },
+    {
+        type: "input",
+        name: "ID",
+        message: "Enter employee name.",
+    },
+    {
+        type: "input",
+        name: "roleProperty",
+        message: "Enter employee name.",
+    },
+
+];
+
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
